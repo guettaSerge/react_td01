1d24cf4b0ef99c87c0d446a4d5472e8e3634de26
serge <ravelomahefasergegustave@gmail.com>
2025-04-07 12:26:25 +0300

    TD01 : Ajouter un footer

--- td-react/src/App.jsx
+++ td-react/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Header.jsx'
 import MainContent from './MainContent.jsx'
+import Footer from './Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,9 +11,7 @@ function App() {
     <>
       <Header />
       <MainContent />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
   )
 }
---
+++ td-react/src/Footer.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function Footer() {
+  return (
+    <>
+      <div>
+        <p>Tous droits réservés - RAVELOMAHEFA Serge Gustave</p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
