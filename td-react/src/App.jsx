f5dff8073d72a04737f5f568bd1dfdd662ea4905
serge <ravelomahefasergegustave@gmail.com>
2025-04-07 12:24:03 +0300

    TD01 : Ajouter un composant pour le contenu principal de la page

--- td-react/src/App.jsx
+++ td-react/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Header.jsx'
+import MainContent from './MainContent.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,14 +9,7 @@ function App() {
   return (
     <>
       <Header />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <MainContent />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ td-react/src/MainContent.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function MainContent() {
+  return (
+    <>
+      <div>
+        <p>Ici, nous afficherons des informations interessantes :) </p>
+      </div>
+    </>
+  )
+}
+
+export default MainContent
